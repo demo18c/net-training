b8d50222449ae7d5dccd8c4fe1de88eaf4c2d08b
RASHAD ABDULBARR <rashadabdulbarr@RASHADs-MacBook-Pro.local>
2021-03-01 20:19:44 -0500

    added environment

--- src/scenes/MajComp.js
+++ src/scenes/MajComp.js
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom';
 import React, { Suspense } from 'react';
 import { Canvas } from 'react-three-fiber';
-import { OrbitControls } from '@react-three/drei';
+import { OrbitControls, Environment } from '@react-three/drei';
 
 import TRX2 from '../models/TRX2RX';
 
@@ -9,9 +9,11 @@ const MajorComponents = () => {
 	return (
 		<Canvas pixelRatio={[1, 2]} camera={{ position: [-5, 0, 0] }}>
 			<OrbitControls />
-			<ambientLight />
+			<ambientLight intensity={1} />
 			<pointLight position={[10, 10, 10]} />
+			<axisHelper />
 			<Suspense fallback={null}>
+				<Environment files="royal_esplanade_1k.hdr" />
 				<TRX2 position={[0, 0, 0]} />
 			</Suspense>
 		</Canvas>
